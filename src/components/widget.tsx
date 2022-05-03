51bb775954764fd5337f4a4db4cff9d0d87b129e
calebetormes <calebetormes@gmail.com>
2022-05-03 18:28:20 -0300

    togglewidgetvisibility

--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -1,11 +1,27 @@
 import {ChatTeardropDots} from 'phosphor-react';
+import { useState } from 'react';
 
 export function Widget() {
+
+  const[isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+  function  toggleWidgetVisibility()  {
+    setIsWidgetOpen(!isWidgetOpen)
+  }
+
   return (
     <div className='absolute bottom-4 right-4'>   
-       <button className='bg-brand-500 rounded-full px-3 h-12 text-white'>
-      <h1><ChatTeardropDots className='w-6 h-6'/></h1>
-    </button>
+    { isWidgetOpen && <p>OpenClose</p>}
+
+      <button onClick={toggleWidgetVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+        <h1><ChatTeardropDots className='w-6 h-6'/></h1>
+        <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
+          <span className='pl-2'></span>
+          FeedBack</span>
+
+      </button>
+
+      
     </div>
   )
 }
