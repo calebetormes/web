aab8dcec22dc23161c6e2e1a02b6500369369cb6
calebetormes <calebetormes@gmail.com>
2022-05-03 11:42:00 -0300

    iniciando componente widget com phosphor-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
+import { Widget } from "./components/widget";
 
-interface ButtonProps {
-  text?: string;
+export function App(){
+  return <Widget/>
 }
-function Button(props: ButtonProps) {
-  return <button className="  bg-violet-500 
-  px-4 
-  rounded 
-  h-16 
-  text-violet-100 
-  hover:bg-violet-700
-  transition-colors;">{props.text ?? 'Defaul'}</button>
-}
-function App() {
-  return(  
-    <div className="flex gap-2">
-      <Button text="Ok"/>
-      <Button text="Enviar"/>
-      <Button />
-    </div>
-  )
-}
-
-export default App
