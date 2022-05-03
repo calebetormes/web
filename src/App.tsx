2aa8693e2e52067db34e0ff7858046816015ff17
calebetormes <calebetormes@gmail.com>
2022-05-03 10:44:23 -0300

    Configuração e aplicação do tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ interface ButtonProps {
   text?: string;
 }
 function Button(props: ButtonProps) {
-  return <button className="bg-violet-500 p-2">{props.text ?? 'Defaul'}</button>
+  return <button className="  bg-violet-500 
+  px-4 
+  rounded 
+  h-16 
+  text-violet-100 
+  hover:bg-violet-700
+  transition-colors;">{props.text ?? 'Defaul'}</button>
 }
 function App() {
   return(  
-    <div>
+    <div className="flex gap-2">
       <Button text="Ok"/>
       <Button text="Enviar"/>
       <Button />
